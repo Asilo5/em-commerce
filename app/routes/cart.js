00a8282dd32255fe2ba8115b248893fc456760e8
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-10-17 19:26:51 -0600

    Add model with an array of prices to display on the page

---
+++ app/routes/cart.js
@@ -0,0 +1,11 @@
+import Route from '@ember/routing/route';
+
+export default class CartRoute extends Route {
+    model() {
+      const items = [
+          {price: 15},
+          {price: 20}
+      ];
+      return items; 
+    }
+}
